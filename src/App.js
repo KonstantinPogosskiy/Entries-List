0d8a4676fc1a185690e1d51263662867d173f339
KonstantinPogosskiy <darcus666@yandex.ru>
2021-12-05 18:52:57 +0300

    refactor: add pagination

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {usePosts} from "./ hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./ hooks/useFetching";
-import {getPageCount} from "./utils/pages";
+import {getPageCount, getPagesArray} from "./utils/pages";
 
 function App() {
     const [posts, setPosts] = useState([]);
@@ -19,6 +19,8 @@ function App() {
     const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    let pagesArray = getPagesArray(totalPages);
+
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
         const response = await PostService.getAll(limit, page)
         setPosts(response.data)
@@ -57,6 +59,11 @@ function App() {
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '100px'}}><Loader/></div>
                 : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Phones List'/>
             }
+            <div className="page__wrapper">
+                {pagesArray.map(p =>
+                    <span onClick={() => setPage(p)} key={p} className={page === p ? 'page page__current' : 'page'}>{p}</span>
+                )}
+            </div>
         </div>
     );
 }
