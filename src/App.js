14194dbe9cdbf43b9ee6b54e1f9fa9a8bf55ce80
KonstantinPogosskiy <darcus666@yandex.ru>
2021-12-10 23:58:53 +0300

    refactor: add autorization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "../src/styles/App.css";
 import {BrowserRouter} from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
@@ -7,7 +7,13 @@ import {AuthContext} from "./context";
 
 function App() {
     const [isAuth, setIsAuth] = useState(false);
-
+    useEffect(() => {
+        if(localStorage.getItem('auth')) {
+            setIsAuth(true)
+        } else {
+            setIsAuth(false)
+        }
+    }, [])
     return (
         <AuthContext.Provider value={{isAuth, setIsAuth}}>
             <BrowserRouter>
